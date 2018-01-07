2044c0bdca255010e3eca90a615397bba8b9ad98
wojciechzarnowiecki <wojciech.zarnowiecki@gmail.com>
2018-01-07 20:31:19 +0100

    api get commit

--- app/src/main/java/wojtek/justyna/numberfacts/MainActivity.java
+++ app/src/main/java/wojtek/justyna/numberfacts/MainActivity.java
@@ -1,13 +1,92 @@
 package wojtek.justyna.numberfacts;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private final String REST_API_NUMBERS = "http://numbersapi.com/random/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        Button button_get = findViewById(R.id.getFacts_button);
+        final TextView result_text = findViewById(R.id.text_result);
+
+        button_get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String newText = getResponse(REST_API_NUMBERS);
+                result_text.setText(newText);
+            }
+        });
+
+
+        Button button_clear = (Button) findViewById(R.id.clearScreen_button);
+
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                result_text.setText("");
+            }
+        });
+
+
+    }
+    public static String getResponse(String url) {
+        InputStream inputStream = null;
+        String result = "";
+
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            URI uri = new URL(url).toURI();
+            HttpResponse httpResponse = httpClient.execute(new HttpGet(uri));
+
+            inputStream = httpResponse.getEntity().getContent();
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Did not work!";
+
+        } catch (Exception e) {
+            Log.d("InputStream", e.getMessage());
+        }
+
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+
     }
 }
